d709dcb2700a54525c66d29dc4d934bfa550615f
Lucas <lucaslemes471@gmail.com>
2026-05-30 17:51:24 -0300

    Reorganizing functions to look like final idea

--- cache/cache.c
+++ cache/cache.c
@@ -1,14 +1,51 @@
 #include<stdio.h>
 #include<math.h>
+#include<stdlib.h>
 #include"../config/config.h"
 #include"../cache/cache.h"
 #include"../utils/utils.h"
 
 void cache_init(Cache* cache, CacheConfig cfg){
+    
+    //incialize parameters
+    cache->hits = 0;
+    cache->misses = 0;
+    cache->accesses = 0;
+    cache->compulsory_misses = 0;
+    cache->capacity_misses = 0;
+    cache->conflict_misses = 0;
 
+    //creating cache lines
+    cache->lines = calloc(
+        cfg.nsets * cfg assoc,
+        sizeof(CacheLine)
+    );
 }
 
-int cache_access(Cache* cache, uint32_t address);
+void simulate(Cache* cache, FILE* benchmark){
+
+    uint32_t c;
+    while ((c = fgetc(benchmark)) != EOF)
+    {
+    cache_mapping(); // ----------MAKE THIS
+
+    cache_substituion(); // ----------MAKE THIS
+    }
+    if(feof(stream))
+    {
+        //verifying output flag
+        if(cache->output_flag == 1){
+            output_print_normalize(cache);
+        }else{
+            printf("CUSTOM OUTPUT");
+        }
+    }
+    else
+    {
+        printf("\nERROR: Can't read file");
+        exit(EXIT_FAILURE);
+    }
+}
 
 void cache_destroy(Cache* cache){
     free(cache->lines);
--- cache/cache.h
+++ cache/cache.h
@@ -8,7 +8,7 @@
 #define CACHE_H
 
 void cache_init(Cache* cache, CacheConfig cfg);
-int cache_access(Cache* cache, uint32_t address);
+void simulate(Cache* cache, FILE* benchmark);
 void cache_destroy(Cache* cache);
 
 #endif //CACHE_H
