61a70e18ff934107c1796a973cda7662907252d8
Lucas <lucaslemes471@gmail.com>
2026-05-30 23:39:21 -0300

    Deleting functions and src

--- functions/cache.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-#include<math.h>
-#include"../src/config.h"
-#include"../src/cache.h"
-
-void build_cache(int nsets, int bsize, int assoc, char substituion_method, bool output_flag, char* input_file){
-
-    process_cache_config(nsets, bsize, assoc, substituion_method,  output_flag, input_file);
-}
