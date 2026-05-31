fd9e0b9d74c49bade08655b6fc7f2c5f4fa1e282
Lucas <lucaslemes471@gmail.com>
2026-05-30 23:36:59 -0300

    making new cache_simulator structure

--- cache_simulator.c
+++ cache_simulator.c
@@ -2,34 +2,33 @@
 #include<stdlib.h>
 #include<math.h>
 #include<stdbool.h>
-#include"src/config.h"
-#include"src/cache.h"
+#include"../config/config.h"
+#include"../cache/cache.h"
+#include"../utils/utils.h"
 
 int main(int argc, char* argv[]){
 	srand(time(NULL));
 
+	//=================== CALL (PART)
 	if (argc != 7){
 		printf("Numero de argumentos incorreto. Utilize:\n");
 		printf("./cache_simulator <nsets> <bsize> <assoc> <substituição> <flag_saida> arquivo_de_entrada\n");
 		exit(EXIT_FAILURE);
 	}
-	int nsets = atoi(argv[1]);
-	int bsize = atoi(argv[2]);
-	int assoc = atoi(argv[3]);
-	char substituion_method = argv[4];
-	bool output_flag = atoi(argv[5]);
-	char* input_file = argv[6];
 
-	printf("nsets = %d\n", nsets);
-	printf("bsize = %d\n", bsize);
-	printf("assoc = %d\n", assoc);
-	printf("subst = %s\n", substituion_method);
-	printf("flagOut = %d\n", output_flag);
-	printf("arquivo = %s\n", input_file);
+	//================== CONFIG (PART)
+	CacheConfig config;
 
-    build_cache(nsets, bsize, assoc, substituion_method, output_flag, input_file);
-    //Parameters: <nsets> <bsize> <assoc> <substituição> <flag_saida> arquivo_de_entrada
+	create_cache_config(config, atoi(argv[1]), atoi(argv[2]), atoi(argv[3]), argv[4][0])
 
+	//================== CACHE (PART)
+	Cache cache;
+
+	cache_init(cache, config, atoi(argv[5]));
+
+	simulate(cache, argv[6]);
+
+	cache_destroy(cache);
 
     return 0;
 }
